97c3935d9c634433c8f210c809e06653ec639de8
Miguel Triana <mikesoft241184@gmail.com>
2019-03-09 01:25:43 -0500

    Sección 5, Clase 20 Desarrollar un componente para mostrar un preloader mientras cargan las zonas

--- App.js
+++ App.js
@@ -1,29 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import AppButton from "./application/AppButton";
+import { Text, View } from 'react-native';
+import AppButton from "./application/components/AppButton";
+import PreLoader from "./application/components/PreLoader";
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-       <AppButton
-           bgColor="rgba(111, 38, 74, 0.7)"
-           title="test"
-           action={() => console.log(1)}
-           iconName="sign-in"
-           iconSize={30}
-           iconColor="#fff"
-       />
-      </View>
+
+        <PreLoader/>
+
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
