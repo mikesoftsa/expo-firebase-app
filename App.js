4944a440572363845e9560996c797d54be75747c
Miguel Triana <mikesoft241184@gmail.com>
2019-03-09 22:31:41 -0500

    Sección 5, Clase 21 Desarrollar un componente para utilizar una imagen de fondo en cada pantalla

--- App.js
+++ App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import AppButton from "./application/components/AppButton";
 import PreLoader from "./application/components/PreLoader";
+import BackgroundImage from "./application/components/BackgroundImage";
 
 
 export default class App extends React.Component {
   render() {
     return (
 
-        <PreLoader/>
+        <BackgroundImage
+          source={require('./assets/images/login-bg.png')}
+        >
+            <Text
+              style={{color: '#fff', marginTop: 100 }}
+            >
+              Hola
+            </Text>
+        </BackgroundImage>
 
     );
   }
