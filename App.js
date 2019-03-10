ceb7ff9cb4dfc6846d36c4614da3493b54085812
Miguel Triana <mikesoft241184@gmail.com>
2019-03-09 23:40:47 -0500

    Sección 6, Clase 23 Iniciar la página principal para mostrar los botones de login y registro

--- App.js
+++ App.js
@@ -3,21 +3,14 @@ import { Text, View } from 'react-native';
 import AppButton from "./application/components/AppButton";
 import PreLoader from "./application/components/PreLoader";
 import BackgroundImage from "./application/components/BackgroundImage";
+import Start from "./application/screens/Start";
 
 
 export default class App extends React.Component {
   render() {
     return (
 
-        <BackgroundImage
-          source={require('./assets/images/login-bg.png')}
-        >
-            <Text
-              style={{color: '#fff', marginTop: 100 }}
-            >
-              Hola
-            </Text>
-        </BackgroundImage>
+        <Start />
 
     );
   }
